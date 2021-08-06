b5abb4ddcb41775dff4a40a082cafbdfd05b768b
junior <juniorlr1407@hotmail.com>
2021-08-06 17:24:02 -0500

    dbscan v2 legend

--- app.py
+++ app.py
@@ -1,39 +1,40 @@
-from flask import Flask, render_template, jsonify, Response
+from flask import Flask, request ,render_template, jsonify, Response
 from flask_cors import CORS
-from main import init, metodo_codo,dbscan_model,transform
+from main import metodo_codo,dbscan_model
 
 app= Flask(__name__)
 CORS(app)
 PORT= 5000
-DEBUG=True
+DEBUG=False
 
 @app.route('/', methods=['GET'])
 def index ():
     return '''<h1>TCS 2021 - RUTAS</h1>
                 <ul>
-                <li>datos</li>
-                <li>metodo_codo</li>
-                <li>transform</li>
                 <li>dbscan_model</li>
                 </ul>'''
-
-@app.route('/datos', methods=['GET'])
-def index2 ():
-    return init()
-
 
 @app.route('/metodo_codo', methods=['GET'])
 def index3 ():
     return metodo_codo()
 
 
-@app.route('/transform', methods=['GET'])
-def index4 ():
-    return transform()  
-
-@app.route('/dbscan_model', methods=['GET'])
+@app.route('/dbscan_model', methods=['GET', 'POST'])
 def index5 ():
-    return dbscan_model() 
+    sql="select distinct o.htitulo_cat, o.htitulo from webscraping w inner join oferta o on (w.id_webscraping=o.id_webscraping) where o.id_estado is null order by 1,2 limit 500;"
+    if request.method == 'POST':
+        body = request.get_json(),
+        print(body)
+        query       = body[0]['query']
+        eps         = body[0]['eps']
+        min_samples = body[0]['min_samples']     
+
+        total_data = dbscan_model(eps, min_samples, query)
+        return (total_data)
+
+    if request.method == 'GET':
+        total_data = dbscan_model(4, 4, sql); 
+        return total_data
 
 if __name__=="__main__":
     app.run(port=PORT, debug=DEBUG)
--- main.py
+++ main.py
@@ -1,75 +1,39 @@
-import json
+import json , io, base64
 import numpy as np
 import matplotlib.pyplot as plt
 import psycopg2
 import pandas as pd
 from sklearn import preprocessing
-from sklearn import cluster
+from sklearn import cluster , metrics
 from sklearn.cluster import DBSCAN
 from matplotlib import pyplot as plt
 from sklearn.neighbors import NearestNeighbors
 
 conn= psycopg2.connect(database="delati", user="modulo4", password="modulo4", host="128.199.1.222", port="5432")
-cursor=conn.cursor()
-sql="select distinct o.htitulo_cat, o.htitulo from webscraping w inner join oferta o on (w.id_webscraping=o.id_webscraping) where o.id_estado is null order by 1,2 limit 500;"
-
-def load_data():
-    cursor.execute(sql)
-    result=cursor.fetchall()
-    return result
-
-def init():
-    data = load_data()
-    json_result = json.dumps(data)
-    return json_result
 
 
-df = pd.read_sql(sql, con = conn)
-#print(df)
-#df.shape
-#DBSCAN_data_scaler = StandardScaler().fit(data)
-#data = DBSCAN_data_scaler.transform(data)
+def get_dataFrame(sql, conn):
+    df = pd.read_sql(sql, con = conn)
+    return df
 
-
-dups = df.groupby(['htitulo_cat']).count()
-#print(dups)
-dups.shape
-
-def transform_data():
+def transform_data(dat):
     label_encoder = preprocessing.LabelEncoder()
-    transformed_data = df.apply(label_encoder.fit_transform)
-    #print(transformed_data)
-    X=transformed_data.iloc[:,[0,1]].values
-    #print(X)
-    return X
+    transformed_data = dat.apply(label_encoder.fit_transform)
+    return transformed_data
 
-def transform():
-    label_encoder = preprocessing.LabelEncoder()
-    transformed_data = df.apply(label_encoder.fit_transform)
-    #print(transformed_data)
-    X=transformed_data.iloc[:,[0,1]].values
-    #print(X)
-    b=[]
-    for item in X:
-        b.append(str(item))
-        print(b)
-    
-    result = json.dumps(b)
-    return result
 
-def metodo_codo():
-    z=transform_data()
+def metodo_codo(dataTransformed):
     # Usamos vecinos más cercanos para calcular la distancia entre puntos.
     # calcular distancias 
     neigh=NearestNeighbors(n_neighbors=2)
-    distance=neigh.fit(z)
+    distance=neigh.fit(dataTransformed)
     # índices y valores de distancia
-    distances,indices=distance.kneighbors(z)
+    distances,indices=distance.kneighbors(dataTransformed)
     # Ahora ordenando el orden creciente de distancia
     sorting_distances=np.sort(distances,axis=0)
     # distancias ordenadas
     sorted_distances=sorting_distances[:,1]
-    return json.dumps(list(sorted_distances))
+    return sorted_distances
 
 #grafico entre distancia vs épsilon
 #plt.plot(sorted_distances)
@@ -77,46 +41,80 @@ def metodo_codo():
 #plt.ylabel('Epsilon')
 #plt.show()
 
-def dbscan_model():
+def dbscan_model(eps, min_samples, query):
+    result = {}
+    #TODO: Obtener data desde la query
+    data=get_dataFrame(query, conn)
+    #TODO: transformamos la data
+    dataTransformed = transform_data(data)
     # inicializamos DBSCAN
-    clustering_model=DBSCAN(eps=4,min_samples=4)
+    clustering_model=DBSCAN(eps=eps,min_samples=min_samples)
     # ajustamos el modelo a transform_data
-    clustering_model.fit(transform_data())
-    #print(clustering_model)
-    # prediccion por DBSCAN
+    clustering_model.fit(dataTransformed)
     predicted_labels=clustering_model.labels_
-    #print(predicted_labels)
-    dat = []
-    for it in predicted_labels:
-        dat.append(int(it))
 
-    res = json.dumps(dat)
+    data['cluster'] = predicted_labels
 
-    cluster=pd.read_json(res)
+    ########metrics and number of clusters####################
+    n_clusters_ = len(set(predicted_labels)) - (1 if -1 in predicted_labels else 0)
+    n_noise_    = list(predicted_labels).count(-1)
+    coefficient = metrics.silhouette_score(dataTransformed, predicted_labels)
 
-    absisa=[]
-    ordenada=[]
-    for item in transform_data():
-        absisa.append(int(item[0]))
-        ordenada.append(int(item[1]))
-        #  b.append(" ".join(str(item)))
+    result['data'] = json.loads(data.to_json(orient = 'records'))
+    result['metricas'] = { 
+                'n_clusters': n_clusters_,
+                 'n_noise': n_noise_,
+                 'Coefficient': coefficient}
 
-    resA = json.dumps(absisa)
-    resO = json.dumps(ordenada)
+    ##############visualizacion de DBSCAN ##################
+#visualzing clusters
 
-    a=pd.read_json(resA)
-    o = pd.read_json(resO)
+    dataTransformed['cluster']=predicted_labels
 
-    df['cluster']=cluster
-    df['absisas']=a
-    df['ordenadas']=o
+    clusters = dataTransformed['cluster'].apply(lambda x: 'cluster ' +str(x+1) if x != -1 else 'outlier')
+    numero_clusters= len(set(clusters))
+    XX=dataTransformed.iloc[:,[0,1]].values
+    plt.figure(figsize=(15,10))
 
-    R = df.to_json(orient="records")
-    return R
-# visualzing clusters
-#plt.scatter(X[:,0], X[:,1],c=predicted_labels, cmap='Paired')
-#plt.xlabel('Categoria')
-#plt.ylabel('Datos')
-#plt.title("DBSCAN")
-#plt.show()
+    for i in range(numero_clusters):
+        if (i-1) != -1:
+            plt.scatter(XX[clustering_model== (i-1), 0], XX[clustering_model==(i-1), 1], s=80, cmap='Paired', label = clusters.unique())
+        else:
+            plt.scatter(XX[clustering_model== (i-1), 0], XX[clustering_model==(i-1), 1], s=80, c='Grey', label = clusters.unique())
+
+    plt.legend(clusters.unique(),bbox_to_anchor=(1.05,1),fontsize=25)
+    plt.grid(color='grey', linestyle='-', linewidth=0.25, alpha=0.6)
+    plt.xlabel('Categoria')
+    plt.ylabel('Datos')
+    plt.title("DBSCAN")
+ 
+    ##############
+    
+    my_stringIObytes = io.BytesIO()
+    plt.savefig(my_stringIObytes, format='jpg')
+    # plt.savefig("graphic2.jpg")
+    my_stringIObytes.seek(0)
+    my_base64_jpgData = base64.b64encode(my_stringIObytes.read())
+    result["graphic_dbscan"] = my_base64_jpgData.decode()
+
+
+    codo=metodo_codo(dataTransformed)
+    #grafico entre distancia vs épsilon
+    plt.plot(codo)
+    plt.xlabel('Distancia')
+    plt.ylabel('Epsilon')
+    plt.title("GRÁFICO MÉTODO DEL CODO")
+    plt.grid(color='grey', linestyle='-', linewidth=0.25, alpha=0.6)
+
+    codo_image = io.BytesIO()
+    plt.savefig(codo_image, format='jpg')
+    codo_image.seek(0)
+    codo_image_decode = base64.b64encode(codo_image.read())
+    result["graphic_method_codo"] = codo_image_decode.decode()
+
+    return result
+
+
+
+
 
