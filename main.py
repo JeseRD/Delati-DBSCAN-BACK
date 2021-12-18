9bc63255c984c44477c742bcfdba534a65825556
Jese Rivas <jese.rivas@unmsm.edu.pe>
2021-12-18 01:31:55 -0500

    intento 8

--- main.py
+++ main.py
@@ -127,14 +127,12 @@ def dbscan_model(eps, min_samples, query):
 
     plt.figure(figsize=(13,10))
 
-    if(numero_clusters != 0):
-
-        for i in range(numero_clusters):
-            if (i-1) != -1:
-                plt.scatter(XX[predicted_labels== (i-1), 0], XX[predicted_labels==(i-1), 1], s=80, cmap='Paired', label = clusters.unique())
-            else:
-                plt.scatter(XX[predicted_labels== (i-1), 0], XX[predicted_labels==(i-1), 1], s=80, c='Grey', label = clusters.unique())
     
+    for i in range(numero_clusters):
+        if (i-1) != -1:
+            plt.scatter(XX[predicted_labels== (i-1), 0], XX[predicted_labels==(i-1), 1], s=80, cmap='Paired', label = clusters.unique())
+        else:
+            plt.scatter(XX[predicted_labels== (i), 0], XX[predicted_labels==(i), 1], s=80, c='Grey', label = clusters.unique())
 
 
     plt.legend(clusters.unique(),bbox_to_anchor=(0.99,1),fontsize=12)
